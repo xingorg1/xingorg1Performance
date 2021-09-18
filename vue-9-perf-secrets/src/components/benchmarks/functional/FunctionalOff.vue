1ba978e71365464b19d1d3714630035ef1cdb52b
guojufeng <guojufeng@jd.com>
2021-09-18 16:59:51 +0800

    9个Vue性能优化技巧

---
+++ vue-9-perf-secrets/src/components/benchmarks/functional/FunctionalOff.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cell">
+    <div v-if="value" class="on">1</div>
+    <section v-else class="off">2</section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+}
+</script>
---
+++ vue-9-perf-secrets/src/components/benchmarks/functional/FunctionalOn.vue
@@ -0,0 +1,6 @@
+<template functional>
+  <div class="cell">
+    <div v-if="props.value" class="on">3</div>
+    <section v-else class="off">4</section>
+  </div>
+</template>
